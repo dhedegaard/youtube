9201d61e1624a5c7cde4aae65123ed0527fce866
Dennis Hedegaard <dennis@dhedegaard.dk>
2015-07-10 20:08:50 +0200

    now shows hours on videos that lasts longer than one hour. Added test as well.

--- youtube/templatetags/pretty_duration.py
+++ youtube/templatetags/pretty_duration.py
@@ -7,4 +7,9 @@ register = template.Library()
 
 @register.filter
 def pretty_duration(value):
-    return time.strftime('%M:%S', time.gmtime(value))
+    time_value = time.gmtime(value)
+
+    if time_value.tm_hour > 0:
+        return time.strftime('%H:%M:%S', time_value)
+
+    return time.strftime('%M:%S', time_value)
--- youtube/tests/test_templatetags.py
+++ youtube/tests/test_templatetags.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 
 from ..templatetags.pretty_duration import pretty_duration
 
+
 class PrettyDurationTest(TestCase):
     def test(self):
         self.assertEqual(pretty_duration(135.0), '02:15')
+
+    def test__with_hour(self):
+        self.assertEqual(pretty_duration(9313.0), '02:35:13')
