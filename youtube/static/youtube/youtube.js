efd09499db54b072cc331eb8adfdc2039955d2d0
Dennis Hedegaard <dennis@dhedegaard.dk>
2015-07-18 16:42:37 +0200

    removed some logging, since this breaks various versions of IE.

--- youtube/static/youtube/youtube.js
+++ youtube/static/youtube/youtube.js
@@ -4,16 +4,10 @@
     "use strict";
 
     $(function() {
-        console.log('proc1');
         $('.thumbnail').error(function() {
-            console.log('proc');
             $(this).hide();
             $(this).unbind();
             $(this).attr('src', '/static/youtube/missing.png');
-        });
-
-        $.each($('.thumbnail'), function(_, e) {
-            console.log(e.complete);
         });
     });
 })();
