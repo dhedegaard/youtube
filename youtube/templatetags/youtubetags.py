b226c7d02097b8a5dc9bf8bb93aa8a4431ebf3bf
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-01-10 17:35:03 +0100

    added better repressentation of how long ago a video was released.

--- youtube/templatetags/youtubetags.py
+++ youtube/templatetags/youtubetags.py
@@ -1,4 +1,9 @@
+import datetime
+
+from dateutil.relativedelta import relativedelta
 from django import template
+from django.utils import timezone
+from django.utils.html import format_html
 
 from ..models import Channel
 
@@ -13,3 +18,19 @@ def visible_channels():
 @register.assignment_tag
 def hidden_channels():
     return Channel.objects.filter(hidden=True).order_by('title')
+
+
+@register.filter
+def timesince_short(fromdate):
+    if not isinstance(fromdate, (datetime.date, datetime.datetime)):
+        raise TypeError("fromdate should be a datetime type.")
+
+    delta = relativedelta(timezone.now(), fromdate)
+    if delta.years > 0:
+        return format_html('{}y{}m ago', delta.years, delta.months)
+    elif delta.months > 0:
+        return format_html('{}m{}D ago', delta.months, delta.days)
+    elif delta.days > 0:
+        return format_html('{}D{}h ago', delta.days, delta.hours)
+    else:
+        return format_html('{}h{}m ago', delta.hours, delta.minutes)
--- youtube/tests/test_templatetags.py
+++ youtube/tests/test_templatetags.py
@@ -1,6 +1,9 @@
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 from django.test import TestCase
 
 from ..templatetags.pretty_duration import pretty_duration
+from ..templatetags.youtubetags import timesince_short
 
 
 class PrettyDurationTest(TestCase):
@@ -9,3 +12,32 @@ class PrettyDurationTest(TestCase):
 
     def test__with_hour(self):
         self.assertEqual(pretty_duration(9313.0), '02:35:13')
+
+
+class TimesinceShortTest(TestCase):
+    def setUp(self):
+        self.now = timezone.now()
+
+    def test__bad_type(self):
+        with self.assertRaises(TypeError):
+            timesince_short('random string')
+
+    def test__more_than_one_year_ago(self):
+        self.assertEqual(
+            u'2y0m ago', timesince_short(
+                self.now - relativedelta(years=2)))
+
+    def test__more_than_one_month_ago(self):
+        self.assertEqual(
+            u'2m0D ago', timesince_short(
+                self.now - relativedelta(months=2)))
+
+    def test__more_than_one_day_ago(self):
+        self.assertEqual(
+            u'2D0h ago', timesince_short(
+                self.now - relativedelta(days=2)))
+
+    def test__hours_and_minutes(self):
+        self.assertEqual(
+            u'2h4m ago', timesince_short(
+                self.now - relativedelta(hours=2, minutes=4)))
